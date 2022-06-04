a71359a476fc95a69e41d7cef70b6a172c6d5293
Peacholino <pia.e.schuster@web.de>
2022-06-04 20:10:24 +0200

    Cows implemented

--- app/src/main/java/com/farmsimulator/models/Cow.kt
+++ app/src/main/java/com/farmsimulator/models/Cow.kt
@@ -1,4 +1,16 @@
 package com.farmsimulator.models
 
 class Cow() : Animal("Cow", 10, listOf("Hay", "Grass", "Carrots", "Corn"), "Moo!") {
+
+    fun milk() {
+        if (sex == "female") {
+            healthIncrease(2)
+            println("I have just been milked! My new health just increased by 2, my new health is $health")
+        } else {
+            healthDecrease(5)
+            println("Im a bull, can't milk me! My health just decreased by 5, my new health is $health")
+        }
+    }
+/* female cows can be milked, which increases their health.
+Male cows cannot be milked and their health decreases if tried */
 }
